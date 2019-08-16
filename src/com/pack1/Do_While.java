cce79d99a3457d0c1bdde7c65bba02728b7a989c
rakeshgit19 <rakeshiiet2010@gmail.com>
2019-08-17 01:08:26 +0530

    my third commit to testing

--- src/com/pack1/Do_While.java
+++ src/com/pack1/Do_While.java
@@ -2,8 +2,10 @@ package com.pack1;
 
 public class Do_While {
 
-	public static void main(String[] args) {
-		int i=1;
+	public static void main(String[] args) 
+	
+	{
+		int i=1;;
 		do
 		{
 			System.out.println(i);
